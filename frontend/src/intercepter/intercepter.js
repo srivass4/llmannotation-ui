3f2fd5307f294c7e92ebdea4ac5fafd4c8d8816e
srivass4 <61455768+srivass4@users.noreply.github.com>
2024-06-12 14:40:24 +0530

    Add files via upload

--- frontend/src/intercepter/intercepter.js
+++ frontend/src/intercepter/intercepter.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 
+// Create an axios instance
+const instance = axios.create();
+
+// Add a request interceptor
+instance.interceptors.request.use((config) => {
+    // Do something before request is sent
+    const token = JSON.parse(localStorage.getItem('token')); // Replace with your token retrieval logic
+    config.headers.Authorization = `bearer ${token}`;
+    return config;
+  }, (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+export default instance;
