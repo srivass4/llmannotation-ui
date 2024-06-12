3eb6fe90797481e7ef52b8db9112ce675da8fe2f
srivass4 <61455768+srivass4@users.noreply.github.com>
2024-06-12 14:33:27 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './assets/css/styles.css';
+import './assets/css/sign-in.css';
+import './assets/css/LoginPage.css';
+import LoginPage from './components/LoginPage';
+import Dashboard from './components/Dashboard';
+import PrivateComponent from './components/PrivateComponent';
+import PageNotFound from './components/PageNotFound';
 
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route element={<PrivateComponent />}>
+          <Route path="/landingannot" element={<Dashboard />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
