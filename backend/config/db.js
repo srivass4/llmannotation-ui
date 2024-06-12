055e3e27d721174b1adef26a223c279c714b3caa
srivass4 <61455768+srivass4@users.noreply.github.com>
2024-06-12 14:28:34 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,17 @@
+const mysql = require('mysql'); 
+const db = mysql.createConnection({
+    host: 'in-cases.ckulv8lpkyxc.ap-southeast-1.rds.amazonaws.com',
+    user: 'svc-incasesdevops',
+    password: 'tXne.upsp2_A3Q%L',
+    database: 'SHUKLAS3'
+})
 
+db.connect((error) => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = db;
